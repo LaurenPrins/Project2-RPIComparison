30b5e4c351ce8967086aaceb405438c64162411b
D0SO <danielptwa@gmail.com>
2021-01-14 16:48:55 +0800

    flask connection

---
+++ app2.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+from flask import Flask, jsonify
+
+
+path = 'postgresql://postgres:postgres@localhost:5432/rpi_comp'
+
+engine = create_engine(path)
+conn = engine.connect()
+
+# Access our DB through pandas dataframe
+data = pd.read_sql("SELECT * FROM rpi", conn).to_json()
+
+# Access database through sql alchemy
+# db = engine.execute('SELECT * FROM "rpi"').fetchall()
+
+# Create Session
+session = Session(engine)
+
+# Create Flask connection
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/v1.0/rpi_comp")
+def regional():
+    """Return the rpi_comp db"""
+
+    return data
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Regional Price Index Analysis!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/rpi_comp"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
